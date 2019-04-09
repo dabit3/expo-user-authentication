495a590fa78add20b67ee854dd882b14a87f36b3
Evan Bacon <baconbrix@gmail.com>
2019-04-08 19:32:04 -0700

    Added web support
    * Updated packages
    * Upgraded to babel 7
    * Upgraded CRNA to Expo-CLI

--- App.js
+++ App.js
@@ -1,33 +1,35 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
-import Amplify, { Auth } from 'aws-amplify'
-import AWSConfig from './aws-exports'
-Amplify.configure(AWSConfig)
+import Amplify, { Auth } from 'aws-amplify';
+import AWSConfig from './aws-exports';
+Amplify.configure(AWSConfig);
 
-import Tabs from './Tabs'
+import Tabs from './Tabs';
 
 export default class App extends React.Component {
   state = {
-    isAuthenticated: false
-  }
-  authenticate(isAuthenticated) {
-    this.setState({ isAuthenticated })
-  }
+    isAuthenticated: false,
+  };
+  authenticate = isAuthenticated => {
+    this.setState({ isAuthenticated });
+  };
   render() {
     if (this.state.isAuthenticated) {
-      console.log('Auth: ', Auth)
+      console.log('Auth: ', Auth);
       return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <View
+          style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+        >
           <Text>Hello {Auth.user.username}!</Text>
         </View>
-      )
+      );
     }
     return (
       <View style={styles.container}>
         <Tabs
           screenProps={{
-            authenticate: this.authenticate.bind(this)
+            authenticate: this.authenticate,
           }}
         />
       </View>
--- Tabs.js
+++ Tabs.js
@@ -1,11 +1,32 @@
-import { TabNavigator } from 'react-navigation'
+import { createBrowserApp } from '@react-navigation/web';
+import { Platform } from 'react-native';
+import { createAppContainer, createBottomTabNavigator } from 'react-navigation';
 
-import SignIn from './SignIn'
-import SignUp from './SignUp'
+import SignIn from './SignIn';
+import SignUp from './SignUp';
 
-const config = {
-  SignIn: { screen: SignIn },
-  SignUp: { screen: SignUp }
-}
+const createApp = Platform.select({
+  web: createBrowserApp,
+  default: createAppContainer,
+});
 
-export default TabNavigator(config)
+export default createApp(
+  createBottomTabNavigator(
+    {
+      SignIn,
+      SignUp,
+    },
+    {
+      tabBarOptions: {
+        labelStyle: {
+          fontSize: 16,
+        },
+        tabStyle: {
+          justifyContent: 'center',
+          flex: 1,
+        },
+        showIcon: false,
+      },
+    },
+  ),
+);
